1089cd4769f0a8e35329290cffd98858a2d07cf6
Shyam-Chen <shyamchen1994@gmail.com>
2018-03-07 11:09:22 +0800

    11th Commit

--- test/test.js
+++ test/test.js
@@ -1,5 +1,13 @@
-describe('E2E Testing', () => {
-  it('TODO', () => {
-    expect(true).toBe(true);
+import request from 'supertest';
+
+const API_URL = 'https://us-central1-web-go-demo.cloudfunctions.net';
+
+describe('API', () => {
+  it('should get hello-world', async () => {
+    const { statusCode, text } = await request(API_URL)
+      .get('/api/hello-world');
+
+    expect(statusCode).toBe(200);
+    expect(text).toBe('Hello, World!');
   });
 });
