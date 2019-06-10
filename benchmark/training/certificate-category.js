b342c52e656ae10026aa16ed15d574db1b5e8a99
linda121084 <linda121084@gmail.com>
2019-06-10 17:18:34 +0800

    feat(training/certificate-category): add certificate-category

---
+++ benchmark/training/certificate-category.js
@@ -0,0 +1,50 @@
+import autocannon from 'autocannon';
+import axios from 'axios';
+
+import { API_URL, RUN_SPEC, report } from './core';
+
+const instructor = async () => {
+  const login = await axios.post(`${API_URL}/permissions/login`, {
+    userName: 'admin',
+    password: 'test',
+  });
+
+  await autocannon({
+    ...RUN_SPEC,
+    title: 'certificate-category',
+    url: API_URL,
+    headers: { token: login.data.DATA.token },
+    requests: [
+      {
+        method: 'POST',
+        path: '/training/certifications/categories/list',
+        body: JSON.stringify({
+          pagingTool: {"currentPage":1,"pageSize":10},"queryCriterias":[],
+          queryOrderBies: [{"columnName":"id","orderType":"desc"}]
+        }),
+      },
+      {
+        method: 'POST',
+        path: '/training/certifications/categories/list',
+        body: JSON.stringify({
+          "pagingTool":{"currentPage":1,"pageSize":10},
+          "queryCriterias":[
+            {"connection":"and","key":"company_id","condition":"=","value":"223","isValueADigital":false},
+            {"connection":"and (","key":"code","condition":"like","value":"270","isValueADigital":false},
+            {"connection":"or","key":"reminder_before_expiration","condition":"like","value":"270","isValueADigital":false},
+            {"connection":")","key":"","condition":"","value":"","isValueADigital":true}
+          ],
+          "queryOrderBies":[{"columnName":"id","orderType":"desc"},
+          ],
+        }),
+      },
+      {
+        method: 'DELETE',
+        path: '/training/certifications/categories/',
+        body: JSON.stringify([71]),
+      },
+    ],
+  }, report);
+};
+
+instructor();
